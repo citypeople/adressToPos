f279f3d177a8f8edcdad906c9c6132c4200525f5
damapic <butakov@damapic.ru>
2014-11-03 21:37:52 +0500

    Revert "Revert "Update adressToPos.snippet.php""
    This reverts commit e86103a080f1d3289135445af124b6833ab8e588.

--- adressToPos.snippet.php
+++ adressToPos.snippet.php
@@ -1,17 +1,34 @@
 <?php
+/*
+name: adressToPos
+Определение координат по адресу.
+Modx Revo.
+YandexMaps.
+Сниппет позволяет на основе введенных данных определить координаты адреса на YandexMaps и записать их в TV параметр.
+Пример вызова снипета. [[!adressToPos? &addTv=maps $adress=[[*Adress]] &city=[[*City]]]]
+Параметры: &addTv - имя TV параметра для записи координат, по умолчанию используется maps $adress - адрес на карте, координаты которого необходимо определить &city - город для более точного определения координат, параметр не обязательный т.к. город можно передавать в параметре $adress
+
+hi@citypeople.ru
+citypeople.ru
+*/
+//Определяем id рессурса
 $id = $modx->resource->id;
 $page = $modx->getObject("modResource", $id);
+
+//Получаем параметры из сниппета
 $city = (isset($city)) ? $city : "Пермь";
 $adress = (isset($adress)) ? $adress : "";
+$addTv = (isset($addTv)) ? $addTv : 'maps';
+// Составляем полный адрес
 $output = $city;
 $output .= $adress; 
-$addTv = (isset($addTv)) ? $addTv : 'maps';
+
 
 
 $params = array(
-    'geocode' => $output, // адрес
-    'format'  => 'json',                          // формат ответа
-    'results' => 1,                               // количество выводимых результатов
+    'geocode' => $output,	// адрес
+    'format'  => 'json',	// формат ответа
+    'results' => 1,			// количество выводимых результатов
 );
 $response = json_decode(file_get_contents('http://geocode-maps.yandex.ru/1.x/?' . http_build_query($params, '', '&')));
  
@@ -23,7 +40,7 @@ else
 {
     $mapPoint = "Ничего не найдено";
 }
-
+// Записываем данные в TV параметр
 if (!$page->setTVValue($addTv, $mapPoint)) {
 $modx->log(xPDO::LOG_LEVEL_ERROR, 'Произошла ошибка при сохранении ТВ');
 }
